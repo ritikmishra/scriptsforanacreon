9358a3109c87a26cf771976812c4700f2d5fe1c5
Ritik Mishra <ritik.mishra314@gmail.com>
2018-07-01 20:36:05 -0500

    Fix attack_on_arrival

--- fraktur_b/attack_on_arrival.py
+++ fraktur_b/attack_on_arrival.py
@@ -7,7 +7,8 @@ import creds
 import time
 
 FLEET_ID = 123
-FLEET_NAME = "125th Fleet"
+FLEET_NAME = "315th Fleet"
+FIND_FLEET_BY_NAME = True
 MODE = "spaceSupremacy"
 
 if __name__ == '__main__':
@@ -18,7 +19,18 @@ if __name__ == '__main__':
     api.get_game_info()
     api.get_objects()
 
-    print("Waiting . . .")
-    time.sleep(api.get_fleet_eta(api.get_obj_by_name(FLEET_NAME)) + 5)
-    fleet_obj = api.get_obj_by_id(FLEET_ID, True)
+    input("Please send your fleet to its destination, and press [ENTER] when ready")
+
+    if FIND_FLEET_BY_NAME:
+        fleet_obj = api.get_obj_by_name(FLEET_NAME)
+    else:
+        fleet_obj = api.get_obj_by_id(FLEET_ID)
+    eta = api.get_fleet_eta(fleet_obj) + 5
+    print("Waiting", eta, "seconds . . .")
+    time.sleep(eta)
+    fleet_obj = api.get_obj_by_id(fleet_obj["id"], True)
+
+    with open('fleetobj.json', 'w') as fp:
+        json.dump(fleet_obj, fp, indent=4, separators=(',', ': '))
+
     api.attack(fleet_obj["anchorObjID"], MODE)
